86a71c0b51ce90092768c2b32421d58454d59be1
msavasci <mehmet_savasci@hotmail.com>
2022-10-27 00:34:42 -0400

    Added main method body for Insertion Sort

--- Week-8/Lecture-16/Sorting/src/insertionsort/Main.java
+++ Week-8/Lecture-16/Sorting/src/insertionsort/Main.java
@@ -1,5 +1,12 @@
 package insertionsort;
 
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        int[] myArray = new int[]{5, 3, 6, 7, 1, 10, 3};
+        int[] sortedArray = InsertionSort.sort(myArray);
+
+        for (int el: sortedArray) {
+            System.out.print(el + " ");
+        }
+    }
 }
